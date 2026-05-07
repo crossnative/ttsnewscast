d19a23d215291fdfd5791d6e850168e4e6ccfa5f
Robert Elles <4633229+robert-elles@users.noreply.github.com>
2026-05-07 10:43:30 +0200

    improve demo

--- backend/newspaper4k_demo.py
+++ backend/newspaper4k_demo.py
@@ -8,6 +8,17 @@ article.parse()
 
 print(article.title)
 print()
+print(article.authors)
+print()
+print(article.publish_date)
+print()
+print(article.top_image)
+
+article.nlp()
+print(article.keywords)
+print(article.summary)
+print()
+
 print(article.text)
 
 text = article.title + "\n" + article.text
